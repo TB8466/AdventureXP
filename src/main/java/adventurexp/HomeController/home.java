5f4cfe040fd6d51727793dafab5ce0e14457f4cf
Elkjaer97 <elkjaer1997@gmail.com>
2021-10-11 12:53:38 +0200

    skrt

---
+++ src/main/java/adventurexp/HomeController/home.java
@@ -0,0 +1,11 @@
+package adventurexp.HomeController;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class home {
+
+    public String displayHome(){
+        return "index";
+    }
+}
