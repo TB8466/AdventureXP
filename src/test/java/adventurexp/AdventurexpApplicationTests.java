ff777dad998158fefcd1b8bb69bc9626555abef0
Thorbjørn <thorbjornbros@gmail.com>
2021-10-11 12:37:35 +0200

    Added

---
+++ src/test/java/adventurexp/AdventurexpApplicationTests.java
@@ -0,0 +1,13 @@
+package adventurexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurexpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
