5b0db668e214ca0ec1c58c61d9b78027dd5f1c72
Thorbjørn <thorbjornbros@gmail.com>
2021-10-13 14:28:09 +0200

    backend works?

--- src/main/java/adventurexp/controller/ActivityController.java
+++ src/main/java/adventurexp/controller/ActivityController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin
 public class ActivityController {
 
 
